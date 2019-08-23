808e6e8f41915c2b916e4f74ee3de5bf7ae41cc4
baglansariev <baglansariev@mail.ru>
2019-08-23 17:45:29 +0600

    slider in process 2

--- public/style/simple-slider/js/simple-slider.js
+++ public/style/simple-slider/js/simple-slider.js
@@ -1,8 +1,35 @@
 $(function () {
+    let sliderImage = $('.slider-image');
+    let viewPort = $('.slider-viewport');
     let sliderWidth = $('.simple-slider').width();
-    let sliderCount = $('.slider-image').length;
-    $('.slider-image').width(sliderWidth);
-    $('.slider-viewport').width(sliderWidth * sliderCount);
+    let sliderCount = sliderImage.length;
+    sliderImage.width(sliderWidth);
+    viewPort.width(sliderWidth * sliderCount);
+    let viewPortWidth = viewPort.width();
+    let left = 0;
+    let right = 0;
+    let maxStep = viewPortWidth - sliderWidth;
 
+    function sliderLeft(){
+        left = left - sliderWidth;
+        if(left < -maxStep){
+            left = 0;
+        }
+        viewPort.animate({marginLeft: left + 'px'}, 1000);
+    }
+
+    function sliderRight(){
+        left = left - sliderWidth;
+        if(left < -maxStep){
+            left = 0;
+        }
+        viewPort.animate({marginRight: left + 'px'}, 1000);
+    }
+
+    // console.log(sliderImage.attr('style'));
+
+    sliderImage.click(function () {
+        sliderRight();
+    });
 
 });
