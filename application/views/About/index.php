70276c878b29447cb1002648baabb1c2a55b3553
baglansariev <baglansariev@mail.ru>
2019-08-29 17:56:53 +0600

    test

--- application/views/About/index.php
+++ application/views/About/index.php
@@ -3,7 +3,10 @@
     <?php echo $page_title; ?>
     <section class="welcome">
         <div class="container">
-
+            <form method="post">
+                <textarea name="int" id="" cols="30" rows="10"></textarea>
+                <input type="submit">
+            </form>
         </div>
     </section>
 </main>
